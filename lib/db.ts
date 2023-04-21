a7a172c54498055e72783fd02db21661518acae8
jfw2020 <jfw2020@ksu.edu>
2023-04-21 16:13:36 -0500

    Small change to db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import sql from "mssql"
 const config: sql.config = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	server: process.env.DB_HOST || `localhost`,
+	server: process.env.DB_HOST || "localhost",
 	database: process.env.DB_DATABASE,
 	port: parseInt( process.env.DB_PORT || "1433" ),
 	options: {
