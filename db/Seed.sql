ca8b0bccba86291dc864888a4359dedd7e853586
Trent Powell <twpowell02@gmail.com>
2023-04-22 09:45:52 -0500

    Finished Table Setup

--- db/Seed.sql
+++ db/Seed.sql
@@ -1,11 +1,81 @@
-INSERT KSUConnect.Users(Username, PasswordHash, FirstName, LastName, ImageUrl)
+/******************
+ * Insert Initial
+ * Values
+ ******************/
+
+-- Static DO NOT ADD
+INSERT KSUConnect.SchoolStatuses([Status])
+VALUES 
+   (N'Freshman'),
+   (N'Sophomore'),
+   (N'Junior'),
+   (N'Senior'),
+   (N'Grad Student'),
+   (N'Alumni'),
+   (N'Faculty')
+
+INSERT KSUConnect.Majors([Name])
 VALUES
-    (N'jfw2020', N'password', N'Jacob', N'Williams', N'https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg'),
-    (N'tpowell', N'password', N'Trent', N'Powell', N'https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg'),
-    (N'whackemer', N'password', N'Will', N'Hackemer', N'https://marketplace.canva.com/EAE2_HrPNRU/1/0/1600w/canva-mascot-character-twitch-profile-picture-jF0b61iv4pQ.jpg');
+    (N'Computer Science'),
+    (N'Computer Engineering'),
+    (N'Undecided'),
+    (N'Cybersecurity'),
+    (N'Mechanical Engineering'),
+    (N'Secondary Education'),
+    (N'Agriculture Economics'),
+    (N'Finance')
+
+INSERT KSUConnect.Categories([Name], MajorId)
+SELECT M.[Name], M.MajorID
+FROM KSUConnect.Majors M
+
+INSERT KSUConnect.Categories([Name])
+VALUES
+    (N'Basketball'),
+    (N'Video Games'),
+    (N'Football'),
+    (N'Greek Life'),
+    (N'Networking'),
+    (N'Engineering')
+-- Static DO NOT ADD
+
+INSERT KSUConnect.Users(Username, PasswordHash, FirstName, LastName, ImageUrl, SchoolStatusID)
+VALUES
+    (N'jfw2020', N'password', N'Jacob', N'Williams', N'https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg', 4),
+    (N'tpowell', N'password', N'Trent', N'Powell', N'https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg', 3),
+    (N'whackemer', N'password', N'Will', N'Hackemer', N'https://marketplace.canva.com/EAE2_HrPNRU/1/0/1600w/canva-mascot-character-twitch-profile-picture-jF0b61iv4pQ.jpg', 2);
 
 INSERT KSUConnect.Posts(UserId, Content)
 VALUES
     (1, N'Had a great time at the KSU Foundation annual ball. Wanted to give a huge shout out to Trent Powell and Will Hackemer for being beasts!'),
     (2, N'Is your engineering team spending too much time building data pipelines instead of focusing on revenue-driving projects?💸 Learn how Fivetran helps thousands of data-driven companies like yours automate their data movement, saving them time and money.📈'),
     (3, N'Our employees take pride in connecting with local students and programs to share insights highlighting the importance of STEM, advancements within the robotics industry, and exploring career paths to go #BeyondTheFinish. Recently, our partners and mentees from the Newton College and Career Academy won the 2023 Georgia state robotics championship, showcasing their dedication for the future of robotics. Thank you to everyone who contributed, and happy #RoboWeek! #SRGGlobal');
+
+INSERT KSUConnect.Followers(FollowerId, FollowingId)
+VALUES
+    (1, 3),
+    (2, 3),
+    (2, 1),
+    (3, 2)
+
+INSERT KSUConnect.UserMajors(UserId, MajorId)
+VALUES
+    (1, 1),
+    (1, 8),
+    (2, 3),
+    (3, 7)
+
+INSERT KSUConnect.UserCategories(UserId, CategoryId)
+VALUES
+    (1, 10),
+    (1, 1),
+    (1, 8),
+    (2, 12),
+    (3, 4)
+
+INSERT KSUConnect.PostCategories(PostId, CategoryId)
+VALUES
+    (1, 13),
+    (2, 14),
+    (3, 5),
+    (3, 2)
--- db/Setup.sql
+++ db/Setup.sql
@@ -1,19 +1,33 @@
-USE [CIS560]; -- Your database here.
+USE [twpowell]; -- Your database here.
 
 /*********************
  * Drop Tables
  *********************/
 
-IF SCHEMA_ID(N'KSUConnect') IS NULL
+IF SCHEMA_Id(N'KSUConnect') IS NULL
    EXEC(N'CREATE SCHEMA [KSUConnect];');
 GO
 
+DROP TABLE IF EXISTS KSUConnect.UserMajors;
+DROP TABLE IF EXISTS KSUConnect.UserCategories;
+DROP TABLE IF EXISTS KSUConnect.PostCategories;
+DROP TABLE IF EXISTS KSUConnect.Categories;
+DROP TABLE IF EXISTS KSUConnect.Majors;
 DROP TABLE IF EXISTS KSUConnect.Posts;
+DROP TABLE IF EXISTS KSUConnect.Followers;
 DROP TABLE IF EXISTS KSUConnect.Users;
+DROP TABLE IF EXISTS KSUConnect.SchoolStatuses;
+GO
 
 /******************
  * Create Tables
  ******************/
+
+CREATE TABLE KSUConnect.SchoolStatuses
+(
+   SchoolStatusId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+   [Status] NVARCHAR(64) NOT NULL UNIQUE
+);
 
 CREATE TABLE KSUConnect.Users
 (
@@ -23,6 +37,8 @@ CREATE TABLE KSUConnect.Users
    FirstName NVARCHAR(32) NOT NULL,
    LastName NVARCHAR(32) NOT NULL,
    ImageUrl NVARCHAR(128) NULL,
+   SchoolStatusId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.SchoolStatuses(SchoolStatusId),
    CreatedOn DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET()),
    UpdatedOn DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET())
 );
@@ -35,4 +51,60 @@ CREATE TABLE KSUConnect.Posts
    Content NVARCHAR(1024) NOT NULL,
    CreatedOn DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET()),
    UpdatedOn DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET())
+);
+
+CREATE TABLE KSUConnect.Followers
+(
+   FollowerId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Users(UserId),
+   FollowingId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Users(UserId),
+   CreatedOn DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET())
+
+   CONSTRAINT PK_Followers PRIMARY KEY
+   (
+      FollowerId,
+      FollowingId
+   )
+);
+
+CREATE TABLE KSUConnect.Majors
+(
+   MajorId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+   [Name] NVARCHAR(64) NOT NULL UNIQUE
+);
+
+CREATE TABLE KSUConnect.Categories
+(
+   CategoryId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+   [Name] NVARCHAR(64) NOT NULL UNIQUE,
+   MajorId INT NULL FOREIGN KEY 
+      REFERENCES KSUConnect.Majors(MajorId)
+);
+
+CREATE TABLE KSUConnect.UserMajors
+(
+   UserMajorId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   UserId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Users(UserId),
+   MajorId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Majors(MajorId)
+);
+
+CREATE TABLE KSUConnect.UserCategories
+(
+   UserCategoriesId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   UserId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Users(UserId),
+   CategoryId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Categories(CategoryId)
+);
+
+CREATE TABLE KSUConnect.PostCategories
+(
+   UserMajorId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   PostId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Posts(PostId),
+   CategoryId INT NOT NULL FOREIGN KEY
+      REFERENCES KSUConnect.Categories(CategoryId)
 );
