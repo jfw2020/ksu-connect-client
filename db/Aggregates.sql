4859829c203d97b29f4cdf562194ae5c2a675acc
will1008m <41597375+whacke@users.noreply.github.com>
2023-04-28 16:33:31 -0500

    Added top 10 word and topic queries

--- db/Aggregates.sql
+++ db/Aggregates.sql
@@ -6,20 +6,26 @@ INNER JOIN KSUConnect.UserCategories UC ON C.CategoryId = UC.CategoryId
 GROUP BY C.Name
 ORDER BY NumberOfPosts DESC, UserCount DESC
 
--- Count users per year
+-- Count users per status
 SELECT SchoolStatuses.Status, COUNT(*) AS CountOfUsers
 FROM KSUConnect.Users
 INNER JOIN KSUConnect.SchoolStatuses ON Users.SchoolStatusId = SchoolStatuses.SchoolStatusId
 GROUP BY SchoolStatuses.Status
 ORDER BY CountOfUsers DESC
 
--- Calculate average amount of followers overall
-SELECT AVG(FollowerCount) AS AvgFollowers
-FROM (
-    SELECT COUNT(*) AS FollowerCount
-    FROM KSUConnect.Followers
-    GROUP BY FollowingId
-) AS FollowerCounts
+-- Shows statistics for when Users joined
+SELECT 
+    DATEPART(YEAR, CreatedOn) AS Year, 
+    DATEPART(MONTH, CreatedOn) AS Month, 
+    COUNT(*) AS UserCount
+FROM KSUConnect.Users
+GROUP BY 
+    DATEPART(YEAR, CreatedOn), 
+    DATEPART(MONTH, CreatedOn)
+ORDER BY 
+    DATEPART(YEAR, CreatedOn) DESC, 
+    DATEPART(MONTH, CreatedOn) DESC
+
 
 -- Rank Users by follower count
 SELECT U.Username, COUNT(F.FollowerId) AS FollowerCount
@@ -27,3 +33,24 @@ FROM KSUConnect.Users U
 LEFT JOIN KSUConnect.Followers F ON U.UserId = F.FollowingId
 GROUP BY U.UserId, U.Username
 ORDER BY FollowerCount DESC
+
+-- Shows top 10 most frequent words
+SELECT TOP 10 Word, COUNT(*) AS Frequency
+FROM (
+    SELECT 
+        value AS Word
+    FROM KSUConnect.Posts
+        CROSS APPLY STRING_SPLIT(Content, ' ')
+) AS Words
+    WHERE LEN(word) > 3 -- exclude short words
+GROUP BY Word
+ORDER BY Frequency DESC;
+
+-- Shows top 10 trending topics from the past week
+SELECT TOP 10 C.Name, COUNT(*) AS Count
+FROM KSUConnect.Categories C
+JOIN KSUConnect.PostCategories PC ON PC.CategoryId = C.CategoryId
+JOIN KSUConnect.Posts P ON P.PostId = PC.PostId
+WHERE P.CreatedOn >= DATEADD(WEEK, -1, GETDATE())
+GROUP BY C.Name
+ORDER BY Count DESC
