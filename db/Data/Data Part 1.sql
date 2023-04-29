3528b9ef45dec51dfbbdf314b38872d832acb37b
jfw2020 <jfw2020@ksu.edu>
2023-04-28 22:27:34 -0500

    Rearranged the repo to match the project code submission canvas assignment

--- db/Data/Data Part 1.sql
+++ db/Data/Data Part 1.sql
@@ -101,3 +101,22 @@ VALUES
     (2, 14),
     (3, 5),
     (3, 2)
+
+-- Update everyone's image URLs
+UPDATE KSUConnect.Users
+SET ImageUrl = N'https://picsum.photos/id/' + CAST(ABS(CHECKSUM(NEWID())) % 100 AS NVARCHAR(3)) + N'/200'
+
+-- Insert Followers with random data
+DECLARE @limit INT = 1;
+DECLARE @userCount INT;
+SELECT @userCount = COUNT(*) FROM KSUConnect.Users;
+
+WHILE @limit <= @userCount
+BEGIN
+    DECLARE @followerId INT = (SELECT TOP 1 UserId FROM KSUConnect.Users ORDER BY NEWID());
+    DECLARE @followingId INT = (SELECT TOP 1 UserId FROM KSUConnect.Users WHERE UserId != @followerId ORDER BY NEWID());
+    
+    INSERT INTO KSUConnect.Followers(FollowerId, FollowingId) VALUES (@followerId, @followingId);
+    
+    SET @limit = @limit + 1;
+END
--- db/ImageUrls.sql
+++
@@ -1,2 +0,0 @@
-UPDATE KSUConnect.Users
-SET ImageUrl = N'https://picsum.photos/id/' + CAST(ABS(CHECKSUM(NEWID())) % 100 AS NVARCHAR(3)) + N'/200'
